3dd578a296873f375db3e374dd3b354173b1b2ac
nazila23 <nazila.pudjakesuma@gmail.com>
2021-10-07 16:36:57 +0700

    pertemuan ke 4

--- 01-03/belajar/mypro/polls/views.py
+++ 01-03/belajar/mypro/polls/views.py
@@ -1,8 +1,16 @@
+# from django.db import models
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
-
-
+from . import models
 def index(request):
-    return HttpResponse("Halooooo indonesia.")
+    if request.POST:
+        # data =request.POST['nama']
+        # print(data)
+        #input data ke database
+        models.polls.objects.create(nama = request.POST['nama'])
+    # nampilinn daata
+    data2 = models.polls.objects.all()
+    return render(request, 'index.html', {'isi':data2})
+
+def about(request):
+    return HttpResponse("Halaman About")
